cbfe54dd0a0b5fb7321bb473061641696775d487
Andrea_Abundis <andrea.abundis@aori.mx>
2017-11-29 17:07:50 -0600

    we-andrea
    Buy terminado

--- Sagan-boilerplate/assets/scripts/logo-change.js
+++ Sagan-boilerplate/assets/scripts/logo-change.js
@@ -1,4 +1,13 @@
-function cambio() {
-    $("gift").toggleClass("inline-elements");    
-    $("gift").toggleClass("gift");    
-  }
+$(document).ready(function () {   
+       $('.img-gift').on('click', function () {
+           $('.gift-ocultar').toggleClass('gift-mostrar');
+          $('.img-gift').toggle();
+       });
+   });
+$(document).ready(function () {   
+       $('.image-membership').on('click', function () {
+           $('.membership-ocultar').toggleClass('membership-mostrar');
+           $('.image-membership').toggle();
+           
+       });
+   });
